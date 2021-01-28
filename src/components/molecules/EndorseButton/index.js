a94558bf86b833c000afe410688fccfe37783588
tekjoe <ramirez.josephh@gmail.com>
2021-01-28 10:38:42 -0600

    added new endorsement form to endorsements page

--- src/components/molecules/EndorseButton/index.js
+++ src/components/molecules/EndorseButton/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import styled from "styled-components"
-import LinkButton from "../../atoms/LinkButton"
+import OutboundLinkButton from "../../atoms/OutboundLinkButton"
 
-const EndorseButton = styled(LinkButton)`
+const EndorseButton = styled(OutboundLinkButton)`
   position: fixed;
   bottom: 2rem;
   right: 2rem;
+  padding: 1rem;
+  background: #f3304a;
   min-width: calc(100% - 4rem);
   display: ${({ isHidden }) => (isHidden ? "none" : "block")};
   @media (min-width: 768px) {
@@ -15,7 +17,11 @@ const EndorseButton = styled(LinkButton)`
 
 export default () => {
   return (
-    <EndorseButton to="/contact?involved=endorse" draggable="false">
+    <EndorseButton
+      href="https://docs.google.com/forms/d/e/1FAIpQLSdtcFHg9GlDFTqgqJ-gebSWJUtuGpk4QSQIhwlMwkHe0PayUw/viewform?gxids=7628"
+      draggable="false"
+      target="_blank"
+    >
       Endorse Jeff
     </EndorseButton>
   )
